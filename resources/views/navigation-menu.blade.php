51ed6a7a11f11fed2ab41e982afbf4e9204e31d1
phurpawangchuk <phurpawangchuk20@gmail.com>
2024-09-01 21:49:49 -0500

    CI/CD add

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -97,7 +97,9 @@
                             <span class="inline-flex rounded-md">
                                 <button type="button"
                                     class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
+                                    @if (Auth::user())
                                     {{ Auth::user()->name }}
+                                    @endif
 
                                     <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none"
                                         viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
@@ -179,13 +181,11 @@
                     <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
                 </div>
             </div>
-
             <div class="mt-3 space-y-1">
                 <!-- Account Management -->
                 <x-responsive-nav-link href="{{ route('profile.show') }}" :active="request()->routeIs('profile.show')">
                     {{ __('Profile') }}
                 </x-responsive-nav-link>
-
                 @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
                 <x-responsive-nav-link href="{{ route('api-tokens.index') }}"
                     :active="request()->routeIs('api-tokens.index')">
